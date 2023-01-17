8b8cce8f4749095b28834c226f1182e0948bf723
Alexey <test@git.ru>
2023-01-17 23:46:12 +0300

    migrations and models

---
+++ database/migrations/2023_01_17_203349_create_filters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filters', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('morphable');
+            $table->string('name');
+            $table->string('db_key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('filters');
+    }
+};
---
+++ database/migrations/2023_01_17_203815_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('morphable');
+            $table->text('text');
+            $table->unsignedBigInteger('reply_id')->nullable();
+            $table->integer('likes')->default(0);
+            $table->integer('dislikes')->default(0);
+            $table->foreignId('coin_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
